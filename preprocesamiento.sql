3a4728101692d6a3da1ea5e459b78262deed3970
vladimir-martin <vladimir.martin@udea.edu.co>
2023-08-30 10:47:40 -0500

    fin de tratamiento inicial, pend delete variables

--- preprocesamiento.sql
+++ preprocesamiento.sql
@@ -2,4 +2,11 @@
 DROP TABLE IF EXISTS Attrition;
 CREATE TABLE Attrition AS
 select t1.EmployeeID, IFNULL(t2.Attrition, "No") AS "Attrition"  FROM manager t1
-left join retirement t2 on t1.EmployeeID=t2.EmployeeID
+left join retirement t2 on t1.EmployeeID=t2.EmployeeID;
+select * from Attrition;
+
+
+--Se eliminan de las tablas las variables que no aportan, segun analisis de categorias
+
+--tabla employe_survey
+select * from employee_survey
